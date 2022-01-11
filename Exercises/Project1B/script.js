0da1830296f7b3b6d772f40bc1e2c86793a0f69c
xtcek <mateusz.greda@tutore.eu>
2022-01-11 12:54:00 +0100

    Enter

---
+++ Exercises/Project1B/script.js
@@ -0,0 +1,11 @@
+function caesar13(str) {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const special = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g
+    let strSplit = str.split("")
+
+    for(let i = 0; i < strSplit.length; i++){
+
+    }
+    
+    // return ''
+}
