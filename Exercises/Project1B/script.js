7006944ca85de61e17213b027838d779804e15e5
xtcek <mateusz.greda@tutore.eu>
2022-01-11 13:50:26 +0100

    Enter

--- Exercises/Project1B/script.js
+++ Exercises/Project1B/script.js
@@ -3,9 +3,12 @@ function caesar13(str) {
 	const special = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g
 	let shift = 13
 	let strSplit = str.split("")
+	let result = ""
 
 	for (let i = 0; i < strSplit.length; i++) {
 		let index = alphabet.indexOf(strSplit[i])
 	}
 	// return ''
 }
+
+console.log(caesar13("PRZEPROGRAMOWANI"))
