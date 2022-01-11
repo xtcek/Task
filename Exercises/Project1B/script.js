e830bc5ec19411e025d9143b04c62d742d7d64b7
xtcek <mateusz.greda@tutore.eu>
2022-01-11 14:24:08 +0100

    Enter

--- Exercises/Project1B/script.js
+++ Exercises/Project1B/script.js
@@ -5,6 +5,13 @@ function caesar13(str) {
 	let strSplit = str.split("")
 	let result = ""
 
+	if (shift > 13) {
+	} else if (typeof str != "string") {
+		return "str have to be string"
+	} else if (typeof shift != "number") {
+		return "shift have to be a number"
+	}
+
 	for (let i = 0; i < strSplit.length; i++) {
 		let index = alphabet.indexOf(strSplit[i])
 	}
