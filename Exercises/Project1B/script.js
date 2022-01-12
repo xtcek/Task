506922d26630cb5fed67fa2c33a5f0a03c4039d1
xtcek <mateusz.greda@tutore.eu>
2022-01-12 17:45:30 +0100

    Enter

--- Exercises/Project1B/script.js
+++ Exercises/Project1B/script.js
@@ -1,28 +1,31 @@
 function validate(itemLength, length) {
-	if(itemLength >= length){
+	if (itemLength >= length) {
 		return itemLength - length
 	}
 	return itemLength
 }
 
 function caesar13(str) {
-	if(typeof str !== 'string' || !str.length){
-		return 'Input is not a string or it is empty'
+	if (typeof str !== "string" || !str.length) {
+		return "Input is not a string or it is empty"
 	}
-	
+
 	const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	let special = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g
-	let strReplace = str.replace(special, "")
-	let strSplit = str.split("")
-	return strSplit.map(el =>{
-		if(!Number.isNaN(+el)){
-			return el
-		}
-		const index = validate(alphabet.indexOf(el) + 13, alphabet.length)
-		return el === el.toUpperCase() ? alphabet[index].toUpperCase() : alphabet[index].toLowerCase()
-	}).join("")
-	
+	const special = /[^a-zA-Z0-9 ]/gi
+	return str.replace(special, '').split('')
+		.map(el => {
+			if (!Number.isNaN(+el)) {
+				return el
+			}
+			const index = validate(alphabet.indexOf(el) + 13, alphabet.length)
+			return el === el.toUpperCase()
+				? alphabet[index].toUpperCase()
+				: alphabet[index].toLowerCase()
+		})
+		.join("")
 }
 
 console.log(caesar13(12345678))
-console.log(caesar13('abddefg'))
+console.log(caesar13("PRZEPROGRAMOWANI"))
+console.log(caesar13("123A$b        cd%^efghij098"))
+console.log(caesar13("32prze()^%progra$#^&MOWANI123"))
