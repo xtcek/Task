f4bc8e2d54be48587b382228d3d966168c486a57
xtcek <mateusz.greda@tutore.eu>
2022-01-12 12:18:17 +0100

    Enter

--- Exercises/Project1B/script.js
+++ Exercises/Project1B/script.js
@@ -2,16 +2,16 @@ function caesar13(str) {
 	const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	let special = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g
 
-	if (shift > 13) {
-	} else if (typeof str != "string") {
-		return "str have to be string"
-	} else if (typeof shift != "number") {
-		return "shift have to be a number"
-	}
+	// if (shift > 13) {
+	// } else if (typeof str != "string") {
+	// 	return "str have to be string"
+	// } else if (typeof shift != "number") {
+	// 	return "shift have to be a number"
+	// }
 
-	for (let i = 0; i < strSplit.length; i++) {
-		let index = alphabet.indexOf(strSplit[i])
-	}
+	// for (let i = 0; i < strSplit.length; i++) {
+	// 	let index = alphabet.indexOf(strSplit[i])
+	// }
 	// return ''
 }
 
