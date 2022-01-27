c0caaeb17d81732013a2dae19735034a07c7a10b
xtcek <mateusz.greda@tutore.eu>
2022-01-27 14:51:28 +0100

    Enter

--- Exercises/Project2B/script.js
+++ Exercises/Project2B/script.js
@@ -1,13 +1,13 @@
 const Cards = {
 	Mastercard: {
 		length: [16],
-		number: [51, 52, 53, 54, 55]
+		number: [51, 52, 53, 54, 55],
 	},
 	Visa: {
 		length: [13, 16],
 		number: [4],
 	},
-	'American Express': {
+	"American Express": {
 		length: [15],
 		number: [34, 37],
 	},
@@ -15,11 +15,12 @@ const Cards = {
 
 const numToString = number => {
 	return number.toString()
-} 
-	
+}
 
 const stringToNumArr = number => {
-	return numToString(number).split("").map(num => +num)
+	return numToString(number)
+		.split("")
+		.map(num => +num)
 }
 
 const filterNumbers = numberArr => {
@@ -29,57 +30,51 @@ const filterNumbers = numberArr => {
 	}
 
 	numberArr.filter((num, idx) => {
-		return filteredNumbers[`${idx % 2 !== 0 ? 'odd' : 'even'}`].push(num)
+		return filteredNumbers[`${idx % 2 !== 0 ? "odd" : "even"}`].push(num)
 	})
 	return filteredNumbers
 }
 // console.log(filterNumbers(stringToNumArr(123456778)));
 
 const calcArraySum = numberArr => {
-	 return numberArr.reduce((prev, next) => (prev += next), 0) 
+	return numberArr.reduce((prev, next) => (prev += next), 0)
 }
 
 const mapArrayNums = numberArr => {
-	return numberArr.map(el => (el * 2 >= 10) ? [1, (el * 2) % 10] : el * 2).flat()
+	return numberArr
+		.map(el => (el * 2 >= 10 ? [1, (el * 2) % 10] : el * 2))
+		.flat()
 }
 
 // console.log(mapArrayNums(stringToNumArr(1234567887654)))
 
 const typeCardNumber = num => {
 	return stringToNumArr(num).slice(0, 2)
-
 }
 // console.log(typeCardNumber(5575060112239323));
 
 const decodeCardType = number => {
 	const cardLength = stringToNumArr(number).length
-	const cardType = typeCardNumber(number).join('')
-	let cardName;
+	const cardType = typeCardNumber(number).join("")
+	let cardName
 
-	for(const card in Cards){
+	for (const card in Cards) {
 		const { length, number } = Cards[card]
-		if(number.includes(cardType) && length.includes(cardLength)){
+		if (number.includes(cardType) && length.includes(cardLength)) {
 			cardName = card
 		}
 	}
 
-
-	console.log(cardLength);
-	console.log(cardType);
+	console.log(cardLength)
+	console.log(cardType)
 
 	return cardName
 }
 
 decodeCardType(4)
 
-
-
-
 function checkCardNumber(number) {
 	const numberArr = stringToNumArr(number)
 
-
-
-	// return 
-	
+	// return
 }
