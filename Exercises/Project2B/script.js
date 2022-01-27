c04c3c50a0c2f71d3ff2e18d40fc93534cba9f36
xtcek <mateusz.greda@tutore.eu>
2022-01-27 22:32:05 +0100

    Enter

--- Exercises/Project2B/script.js
+++ Exercises/Project2B/script.js
@@ -70,10 +70,10 @@ const decodeCardType = number => {
 	for (const card in Cards) {
 		const { length, number } = Cards[card]
 		const startsWith = number.map(el => cardType.toString().startsWith(el))
-		// if (number.includes(cardType) && length.includes(cardLength)) {
-		// 	cardName = card
-		// 	break
-		// }
+		if (number.includes(cardType) && length.includes(cardLength)) {
+			cardName = card
+			break
+		}
 	}
 
 	// return cardName
