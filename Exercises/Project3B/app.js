67069515d127910bd50494f1a9d93b1fa1d03e34
xtcek <mateusz.greda@tutore.eu>
2022-03-14 15:48:34 +0100

    calcBMI func

--- Exercises/Project3B/app.js
+++ Exercises/Project3B/app.js
@@ -5,9 +5,12 @@ const addTo =
 	"?utm_source=Testy&utm_medium=Pierwsz&utm_campaign=Lato&utm_id=MateuszGr%C4%99da"
 console.log(addTo)
 
-const input1 = document.getElementById("num1").value
-const input2 = document.getElementById("num2").value
+// const input1 = document.getElementById("num1").value
+// const input2 = document.getElementById("num2").value
 
-console.log(input1)
+const calcBMI = (weight, height) => {
+	const BMI = weight / height ** 2
+    return BMI;
+}
 
-const calcBMI = () => {}
+console.log(calcBMI(175, 120));
