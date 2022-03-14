f0b67cf397f2f979ac58195f02ca8c78732a25a6
xtcek <mateusz.greda@tutore.eu>
2022-03-14 15:28:11 +0100

    get Input values and assign to const

--- Exercises/Project3B/app.js
+++ Exercises/Project3B/app.js
@@ -4,3 +4,10 @@ const addTo =
 	url +
 	"?utm_source=Testy&utm_medium=Pierwsz&utm_campaign=Lato&utm_id=MateuszGr%C4%99da"
 console.log(addTo)
+
+const input1 = document.getElementById("num1").value
+const input2 = document.getElementById("num2").value
+
+console.log(input1)
+
+const calcBMI = () => {}
