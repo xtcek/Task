28461c56aacfb8a40d546154ac075137cf5e19dc
xtcek <mateusz.greda@tutore.eu>
2022-03-16 15:59:22 +0100

    Add another query URL params and replace

--- Exercises/Project3B/app.js
+++ Exercises/Project3B/app.js
@@ -1,5 +1,20 @@
-// Add query params to the URL
 
+const subSite1 = document.querySelector(".subsite1")
+
+// Event handler for change query URL params
+subSite1.addEventListener("click", function (e) {
+	e.preventDefault()
+
+	window.location.href =
+		subSite1.href +
+		"?przejscie" +
+		"?utm_source=Testy&utm_medium=Pierwsz&utm_campaign=Lato&utm_id=MateuszGr%C4%99da".replace(
+			"utm_campaign=Lato",
+			"utm_campaign=Zima"
+		)
+})
+
+// Add query params to the URL
 history.pushState(
 	null,
 	null,
