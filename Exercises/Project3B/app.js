bfe8bc046100da16e0e98a6f218e062db4910e9b
xtcek <mateusz.greda@tutore.eu>
2022-03-16 16:42:08 +0100

    Few changes with event handlers and query params

--- Exercises/Project3B/app.js
+++ Exercises/Project3B/app.js
@@ -1,25 +1,29 @@
-
 const subSite1 = document.querySelector(".subsite1")
+const subSite2 = document.querySelector(".subsite2")
 
 // Event handler for change query URL params
 subSite1.addEventListener("click", function (e) {
 	e.preventDefault()
-
-	window.location.href =
-		subSite1.href +
-		"?przejscie" +
-		"?utm_source=Testy&utm_medium=Pierwsz&utm_campaign=Lato&utm_id=MateuszGr%C4%99da".replace(
-			"utm_campaign=Lato",
-			"utm_campaign=Zima"
-		)
+	if (subSite1)
+		window.location.href =
+			subSite1.href +
+			"?przejscie" +
+			"?utm_source=Testy&utm_medium=Pierwsz&utm_campaign=Lato&utm_id=MateuszGr%C4%99da".replace(
+				"utm_campaign=Lato",
+				"utm_campaign=Zima"
+			)
 })
 
-// Add query params to the URL
 history.pushState(
 	null,
 	null,
 	"?utm_source=Testy&utm_medium=Pierwsz&utm_campaign=Lato&utm_id=MateuszGr%C4%99da"
 )
+
+// subSite2.addEventListener("click", function (e) {
+//     e.preventDefault();
+// 	window.location.href.replace(window.location.search, "")
+// })
 
 // const input1 = document.getElementById("num1").value
 // const input2 = document.getElementById("num2").value
