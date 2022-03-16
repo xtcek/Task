2d5471cba3d838211cc935df29001af7d93f11e2
xtcek <mateusz.greda@tutore.eu>
2022-03-16 15:48:40 +0100

    Add query params to the URL

--- Exercises/Project3B/app.js
+++ Exercises/Project3B/app.js
@@ -1,16 +1,17 @@
-const url = document.URL
-console.log(url)
-const addTo =
-	url +
+// Add query params to the URL
+
+history.pushState(
+	null,
+	null,
 	"?utm_source=Testy&utm_medium=Pierwsz&utm_campaign=Lato&utm_id=MateuszGr%C4%99da"
-console.log(addTo)
+)
 
 // const input1 = document.getElementById("num1").value
 // const input2 = document.getElementById("num2").value
 
 const calcBMI = (weight, height) => {
 	const BMI = weight / height ** 2
-    return BMI;
+	return BMI
 }
 
-console.log(calcBMI(175, 120));
+console.log(calcBMI(175, 120))
