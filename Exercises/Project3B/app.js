865f4f61b1fa3f693e1c57c9bb2e8c5c164f1cbc
xtcek <mateusz.greda@tutore.eu>
2022-03-24 19:24:00 +0100

    changeURL function with first block of code

--- Exercises/Project3B/app.js
+++ Exercises/Project3B/app.js
@@ -1,3 +1,13 @@
+const changeURL = () => {
+	let path = window.location.pathname
+	console.log(path)
+	if (path === "/Project1/index.html") {
+		window.history.pushState(
+			null,
+			null,
+			"?utm_source=Testy&utm_medium=Pierwsz&utm_campaign=Lato&utm_id=MateuszGr%C4%99da"
+		)
+}
 
 const calcBMI = (weight, height) => {
 	let bmi = (weight / Math.pow(height / 100, 2)).toFixed(2)
