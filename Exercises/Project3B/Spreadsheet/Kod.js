390c6ba3befd2e6cdf569d12bdde6dca0a56ef7a
xtcek <mateusz.greda@tutore.eu>
2022-03-24 19:23:11 +0100

    Add npm Google Apps Script to Project and clear js

---
+++ Exercises/Project3B/Spreadsheet/Kod.js
@@ -0,0 +1,10 @@
+let ss = SpreadsheetApp.getActiveSpreadsheet()
+let sheetTask2 = ss.getSheetByName("Zadanie 2")
+let sheetTask3 = ss.getSheetByName("Zadanie 3")
+
+function myFunction() {
+	let lastRow = sheetTask3.getLastRow();
+
+	if(sheetTask3.getRange(lastRow, 1) !== '') sheetTask3.getRange(lastRow, 1).setValue('TEST')
+
+}
--- Exercises/Project3B/app.js
+++ Exercises/Project3B/app.js
@@ -1,30 +1,3 @@
-const subSite1 = document.querySelector(".subsite1")
-// Event handler for change query URL params
-subSite1.addEventListener("click", function (e) {
-	e.preventDefault()
-	if (subSite1)
-		window.location.href =
-			subSite1.href +
-			"?przejscie" +
-			"?utm_source=Testy&utm_medium=Pierwsz&utm_campaign=Lato&utm_id=MateuszGr%C4%99da".replace(
-				"utm_campaign=Lato",
-				"utm_campaign=Zima"
-			)
-})
-
-history.pushState(
-	null,
-	null,
-	"?utm_source=Testy&utm_medium=Pierwsz&utm_campaign=Lato&utm_id=MateuszGr%C4%99da"
-)
-
-// subSite2.addEventListener("click", function (e) {
-//     e.preventDefault();
-// 	window.location.href.replace(window.location.search, "")
-// })
-
-// const input1 = document.getElementById("num1").value
-// const input2 = document.getElementById("num2").value
 
 const calcBMI = (weight, height) => {
 	let bmi = (weight / Math.pow(height / 100, 2)).toFixed(2)
