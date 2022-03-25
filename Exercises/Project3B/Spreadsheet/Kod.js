0b8c9029d4ecb753114ed9e2f736d4fee2e41796
xtcek <mateusz.greda@tutore.eu>
2022-03-25 15:29:00 +0100

    .

--- Exercises/Project3B/Spreadsheet/Kod.js
+++ Exercises/Project3B/Spreadsheet/Kod.js
@@ -3,8 +3,8 @@ let sheetTask2 = ss.getSheetByName("Zadanie 2")
 let sheetTask3 = ss.getSheetByName("Zadanie 3")
 
 function myFunction() {
-	let lastRow = sheetTask3.getLastRow();
+	let lastRow = sheetTask3.getLastRow()
 
-	if(sheetTask3.getRange(lastRow, 1) !== '') sheetTask3.getRange(lastRow, 1).setValue('TEST')
-
+	if (sheetTask3.getRange(lastRow, 1) !== "")
+	sheetTask3.getRange(lastRow, 1).setValue("TEST123")
 }
